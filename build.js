b579e434ae41c7eee71b9bbf1d97d9d74c8e8a2b
Gabriele Nataneli <gabrielenataneli@yahoo.com>
2024-11-16 22:07:27 -0800

    Fixed build script - successfully resolved issues running the build script - enclosed the build step in a try/catch block - added 'package-lock.json'

--- build.js
+++ build.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
-import * as esbuild from 'esbuild'
-import bookmarkletPlugin from 'esbuild-plugin-bookmarklet'
+import * as esbuild from 'esbuild';
+import bookmarkletPlugin from 'esbuild-plugin-bookmarklet';
 
-await esbuild.build({
-  bundle: true,
-  entryPoints: ['gpt2md.js'],
-  format: 'iife',
-  minify: true,
-  outfile: 'dist/gpt2md.bookmarklet.js',
-  plugins: [bookmarkletPlugin],
-  sourcemap: false,
-  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-  write: false,
-})
+try {
+	await esbuild.build({
+	  bundle: true,
+	  entryPoints: ['gpt2md.js'],
+	  format: 'iife',
+	  minify: true,
+	  outfile: 'dist/gpt2md.bookmarklet.js',
+	  plugins: [bookmarkletPlugin],
+	  sourcemap: false,
+	  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+	  write: false,
+	});
+	console.log('Build completed successfully.');
+  } catch (error) {
+	console.error('Build failed:', error);
+  }
